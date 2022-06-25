4995a5beeb0be37b4b582b24219b155d60503fee
antidods <andrey_34rus@mail.ru>
2022-06-25 20:37:41 +0300

    add railFenceCipher

--- js/railFenceCipher.js
+++ js/railFenceCipher.js
@@ -3,39 +3,93 @@
 //https://www.codewars.com/kata/58c5577d61aefcf3ff000081
 
 function encodeRailFenceCipher(string, numberRails) {
-  const array = string.split("");
-  let encode = "",
-    obj = {},
-    direction = true,
-    step = 1;
+   const array = string.split("");
+   let encode = "",
+      obj = [],
+      direction = true,
+      step = 0;
 
-  for (let rail = numberRails; rail > 0; rail--) obj[rail] = [];
+   for (let rail = 0; rail < numberRails; rail++) obj[rail] = [];
 
-  for (let elem = 0; elem < array.length; elem++) {
-    obj[step].push(array[elem]);
+   for (let elem = 0; elem < array.length; elem++) {
+      obj[step].push(array[elem]);
 
-    if (direction) step++;
-    else step--;
+      if (direction) step++;
+      else step--;
 
-    if (step > numberRails) {
-      step -= 2;
-      direction = false;
-    }
+      if (step === numberRails) {
+         step -= 2;
+         direction = false;
+      }
 
-    if (step === 0) {
-      step += 2;
-      direction = true;
-    }
-  }
+      if (step < 0) {
+         step += 2;
+         direction = true;
+      }
+   }
 
-  for (let rail = 1; rail <= numberRails; rail++) encode += obj[rail].join("");
+   for (let rail = 0; rail < numberRails; rail++) encode += obj[rail].join("");
 
-  return encode;
+   return encode;
 }
 
 function decodeRailFenceCipher(string, numberRails) {
-  
+   const array = string.split("");
+   let decode = "";
+   let obj = [];
+   let direction = true;
+   let step = 0;
+   let rail = 0;
 
+   for (let Rail = 0; Rail < numberRails; Rail++) obj[Rail] = [];
+
+   for (let elem = 0; elem < array.length; elem++) {
+      obj[step].push(array[elem]);
+
+      if (direction) step++;
+      else step--;
+
+      if (step === numberRails) {
+         step -= 2;
+         direction = false;
+      }
+
+      if (step < 0) {
+         step += 2;
+         direction = true;
+      }
+   }
+   console.log(obj);
+
+   step = 0;
+
+   for (let index = 0; index < array.length; index++) {
+      obj[rail][step] = array[index];
+      step++;
+      if (!obj[rail][step]) {
+         step = 0;
+         rail++;
+      }
+   }
+
+   for (let elem = 0; elem < array.length; elem++) {
+      decode += obj[step][0];
+      obj[step].shift();
+
+      if (direction) step++;
+      else step--;
+
+      if (step === numberRails) {
+         step -= 2;
+         direction = false;
+      }
+
+      if (step < 0) {
+         step += 2;
+         direction = true;
+      }
+   }
+
+   return decode;
 }
-
 
