1c16eca1572fd0a31948fee547f4729930f7ed73
Andrey <andrey_34rus@mail.ru>
2022-07-03 18:42:24 +0300

    working on calc

--- js/EvaluateMathematicalExpression.js
+++ js/EvaluateMathematicalExpression.js
@@ -1,7 +1,68 @@
-"use strict";
-
 //https://www.codewars.com/kata/52a78825cdfc2cfc87000005
 
-let calc = function (expression) {
-  return 2;
-};
+function calc(expression) {
+  let inArray = [];
+  let tempNum = "";
+
+  for (let i = 0; i < expression.length; i++) {
+    const symbol = expression[i];
+
+    if (
+      symbol == "(" ||
+      symbol == ")" ||
+      symbol == "+" ||
+      symbol == "-" ||
+      symbol == "*" ||
+      symbol == "/"
+    ) {
+      if (tempNum) {
+        inArray.push(tempNum);
+        tempNum = "";
+      }
+      inArray.push(symbol);
+    } else if (
+      symbol == "1" ||
+      symbol == "2" ||
+      symbol == "3" ||
+      symbol == "4" ||
+      symbol == "5" ||
+      symbol == "6" ||
+      symbol == "7" ||
+      symbol == "8" ||
+      symbol == "9" ||
+      symbol == "0"
+    ) {
+      tempNum += symbol;
+      if (i === expression.length - 1) inArray.push(symbol);
+    }
+  }
+
+  function calcTwo(first, operator, last) {
+    let result;
+    switch (operator) {
+      case "+":
+        result = Number(first) + Number(last);
+        break;
+
+      case "-":
+        result = Number(first) - Number(last);
+        break;
+
+      case "/":
+        result = Number(first) / Number(last);
+        break;
+
+      case "*":
+        result = Number(first) * Number(last);
+        break;
+
+      default:
+        break;
+    }
+    return result;
+  }
+
+  return calcTwo(inArray[0], inArray[1], inArray[2]);
+}
+
+calc("6+3+756/3");
--- test/EvaluateMathematicalExpressionTest.js
+++ test/EvaluateMathematicalExpressionTest.js
@@ -19,47 +19,34 @@ describe("Добавляем скобки, 1 уровень вложенност
 });
 
 describe("Несколько уровней вложенности", () => {
-  it(`values: "((1+1))"`, () => assert.strictEqual(calc("((1+1))"), 1)); 
+  it(`values: "((1+1))"`, () => assert.strictEqual(calc("((1+1))"), 2));
   it(`values: "(1-(1))"`, () => assert.strictEqual(calc("(1-(1))"), 0));
   it(`values: "((1)*1)"`, () => assert.strictEqual(calc("((1)*1)"), 1));
   it(`values: "1/((1))"`, () => assert.strictEqual(calc("1/((1))"), 1));
-  it(`values: "((1+1)+(1+1))"`, () => assert.strictEqual(calc("((1+1)+(1+1))"), 4));
+  it(`values: "((1+1)+(1+1))"`, () =>
+    assert.strictEqual(calc("((1+1)+(1+1))"), 4));
   it(`values: "(5+(6))"`, () => assert.strictEqual(calc("(5+(6))"), 11));
   it(`values: "((9)+(8))"`, () => assert.strictEqual(calc("((9)+(8))"), 17));
 });
 
 describe("С плавающей точкой", () => {
   it(`values: "5.456+56"`, () => assert.strictEqual(calc("5.456+56"), 61.456));
-  it(`values: "7.33 * 1.2"`, () => assert.strictEqual(calc("7.33 * 1.2"), 8.796));
-  it(`values: "1.333333*3.269"`, () => assert.strictEqual(calc("1.333333*3.269"), 4.358665577));
-
+  it(`values: "7.33 * 1.2"`, () =>
+    assert.strictEqual(calc("7.33 * 1.2"), 8.796));
+  it(`values: "1.333333*3.269"`, () =>
+    assert.strictEqual(calc("1.333333*3.269"), 4.358665577));
 });
 
-// describe("Tests", () => {
-//   it(`values: ""`, () => assert.strictEqual(calc(""), false));
-//   it(`values: ""`, () => assert.strictEqual(calc(""), false));
-//   it(`values: ""`, () => assert.strictEqual(calc(""), false));
-//   it(`values: ""`, () => assert.strictEqual(calc(""), false));
-//   it(`values: ""`, () => assert.strictEqual(calc(""), false));
-//   it(`values: ""`, () => assert.strictEqual(calc(""), false));
-//   it(`values: ""`, () => assert.strictEqual(calc(""), false));
-// });
-
-describe("Tests", () => {
-  it("test", () => {
-    const tests = [
-      ["1+1", 2],
-      ["1 - 1", 0],
-      ["1* 1", 1],
-      ["1 /1", 1],
-      ["-123", -123],
-      ["123", 123],
-      ["2 /2+3 * 4.75- -6", 21.25],
-      ["12* 123", 1476],
-      ["2 / (2 + 3) * 4.33 - -6", 7.732],
-    ];
-
-    for (const [input, expected] of tests)
-      Test.assertEquals(calc(input), expected);
-  });
+describe("Codewars tests", () => {
+  it(`values: "1+1"`, () => assert.strictEqual(calc("1+1"), 2));
+  it(`values: "1 - 1"`, () => assert.strictEqual(calc("1 - 1"), 0));
+  it(`values: "1* 1"`, () => assert.strictEqual(calc("1* 1"), 1));
+  it(`values: "1 /1"`, () => assert.strictEqual(calc("1 /1"), 1));
+  it(`values: "-123"`, () => assert.strictEqual(calc("-123"), -123));
+  it(`values: "123"`, () => assert.strictEqual(calc("123"), 123));
+  it(`values: "2 /2+3 * 4.75- -6"`, () =>
+    assert.strictEqual(calc("2 /2+3 * 4.75- -6"), 21.25));
+  it(`values: "12* 123"`, () => assert.strictEqual(calc("12* 123"), 1476));
+  it(`values: "2 / (2 + 3) * 4.33 - -6"`, () =>
+    assert.strictEqual(calc("2 / (2 + 3) * 4.33 - -6"), 7.732));
 });
