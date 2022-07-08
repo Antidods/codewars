045c38eac75047aeb74f46291908d3092204daa8
Andrey <100022449+Antidods@users.noreply.github.com>
2022-07-08 22:12:01 +0300

    Update EvaluateMathematicalExpression.js

--- js/EvaluateMathematicalExpression.js
+++ js/EvaluateMathematicalExpression.js
@@ -1,133 +1,157 @@
 //https://www.codewars.com/kata/52a78825cdfc2cfc87000005
 
 function calc(expression) {
-  let inArray = [];
-  let tempNum = "";
+   let inArray = [];
+   let tempNum = "";
 
-  for (let i = 0; i < expression.length; i++) {
-    console.log(inArray);
-    let symbol = expression[i];
-    if (symbol === ",") symbol = ".";
-    if (symbol === "-" && i === 0) {
-      tempNum += symbol;
-      continue;
-    }
-
-    if (symbol === " ") continue;
-
-    if (
-      symbol == "(" ||
-      symbol == ")" ||
-      symbol == "+" ||
-      symbol == "-" ||
-      symbol == "*" ||
-      symbol == "/"
-    ) {
-      if (tempNum) {
-        inArray.push(Number(tempNum));
-        tempNum = "";
+   for (let i = 0; i < expression.length; i++) {
+      let symbol = expression[i];
+      if (symbol === ",") symbol = ".";
+      if (symbol === "-" && i === 0) {
+         tempNum += symbol;
+         continue;
       }
-      inArray.push(symbol);
-    } else if (
-      symbol == "1" ||
-      symbol == "2" ||
-      symbol == "3" ||
-      symbol == "4" ||
-      symbol == "5" ||
-      symbol == "6" ||
-      symbol == "7" ||
-      symbol == "8" ||
-      symbol == "9" ||
-      symbol == "0" ||
-      symbol == "."
-    ) {
-      tempNum += symbol;
-      if (i === expression.length - 1) {
-        inArray.push(Number(tempNum));
+
+      if (
+         symbol == "(" ||
+         symbol == ")" ||
+         symbol == "+" ||
+         symbol == "-" ||
+         symbol == "*" ||
+         symbol == "/"
+      ) {
+         if (tempNum) {
+            inArray.push(tempNum);
+            tempNum = "";
+         }
+
+         if (
+            (symbol == "-" && inArray.at(-1) === "(") ||
+            inArray.at(-1) === ")" ||
+            inArray.at(-1) === "+" ||
+            inArray.at(-1) === "-" ||
+            inArray.at(-1) === "*" ||
+            inArray.at(-1) === "/"
+         ) {
+            tempNum += symbol;
+            continue;
+         }
+
+         inArray.push(symbol);
+      } else if (
+         symbol == "1" ||
+         symbol == "2" ||
+         symbol == "3" ||
+         symbol == "4" ||
+         symbol == "5" ||
+         symbol == "6" ||
+         symbol == "7" ||
+         symbol == "8" ||
+         symbol == "9" ||
+         symbol == "0" ||
+         symbol == "."
+      ) {
+         tempNum += symbol;
+         if (i === expression.length - 1) {
+            inArray.push(tempNum);
+         }
       }
-    }
-  }
+   }
 
-  if (inArray.length === 1) return Number(inArray[0]);
+   if (inArray.length === 1) return Number(inArray[0]);
 
-  function calcTwo(first, operator, last) {
-    let result;
-    switch (operator) {
-      case "+":
-        result = Number(first) + Number(last);
-        break;
+   function calcTwo(first, operator, last) {
+      let result;
+      switch (operator) {
+         case "+":
+            result = Number(first) + Number(last);
+            break;
 
-      case "-":
-        result = Number(first) - Number(last);
-        break;
+         case "-":
+            result = Number(first) - Number(last);
+            break;
 
-      case "/":
-        result = Number(first) / Number(last);
-        break;
+         case "/":
+            result = Number(first) / Number(last);
+            break;
 
-      case "*":
-        result = Number(first) * Number(last);
-        break;
+         case "*":
+            result = Number(first) * Number(last);
+            break;
 
-      default:
-        break;
-    }
-    return result;
-  }
-
-  function calcMany(array) {
-    for (let i = 1; i < array.length; i++) {
-      if (array.length < 2) break;
-      if (array[i] === "*" || array[i] === "/") {
-        array.splice([i - 1], 3, calcTwo(array[i - 1], array[i], array[i + 1]));
-        i--;
+         default:
+            break;
       }
-      if (array.length < 2) return array[0];
-    }
+      return result;
+   }
 
-    for (let i = 1; i < array.length; i++) {
-      if (array.length < 2) break;
-      if (array[i] === "+" || array[i] === "-") {
-        array.splice([i - 1], 3, calcTwo(array[i - 1], array[i], array[i + 1]));
-        i--;
+   function calcMany(array) {
+      for (let i = 1; i < array.length; i++) {
+         if (array.length < 2) break;
+         if (array[i] === "*" || array[i] === "/") {
+            array.splice(
+               [i - 1],
+               3,
+               calcTwo(array[i - 1], array[i], array[i + 1])
+            );
+            i--;
+         }
+         if (array.length < 2) return array[0];
       }
-      if (array.length < 2) return array[0];
-    }
 
-    return Number(array);
-  }
-
-  console.log(inArray);
-
-  console.log(inArray.indexOf("("));
-
-  let firstBracket = -1;
-  let lastBracket = -1;
-  x = 5;
-
-  while (x > 0) {
-    //inArray.indexOf("(") > -1
-
-    firstBracket = -1;
-    lastBracket = -1;
-
-    for (let i = 0; i < inArray.length; i++) {
-      const element = inArray[i];
-      if (element === "(") firstBracket = i;
-      if (element === ")") {
-        lastBracket = i;
-        break;
+      for (let i = 1; i < array.length; i++) {
+         if (array.length < 2) break;
+         if (array[i] === "+" || array[i] === "-") {
+            array.splice(
+               [i - 1],
+               3,
+               calcTwo(array[i - 1], array[i], array[i + 1])
+            );
+            i--;
+         }
+         if (array.length < 2) return array[0];
       }
-    }
 
-    if (firstBracket > -1 && lastBracket > -1) {
-      const promResult = calcMany(inArray.slice(firstBracket + 1, lastBracket));
-      inArray.splice(firstBracket, lastBracket - firstBracket - 1, promResult);
-    }
-    x--;
-  }
+      return Number(array);
+   }
 
-  return calcMany(inArray);
+   console.log(inArray);
+
+   console.log(inArray.indexOf("("));
+
+   let firstBracket = -1;
+   let lastBracket = -1;
+   x = 5;
+
+   while (x > 0) {
+      //inArray.indexOf("(") > -1
+
+      firstBracket = -1;
+      lastBracket = -1;
+
+      for (let i = 0; i < inArray.length; i++) {
+         const element = inArray[i];
+         if (element === "(") firstBracket = i;
+         if (element === ")") {
+            lastBracket = i;
+            break;
+         }
+      }
+
+      if (firstBracket > -1 && lastBracket > -1) {
+         const promResult = calcMany(
+            inArray.slice(firstBracket + 1, lastBracket)
+         );
+         inArray.splice(
+            firstBracket,
+            lastBracket - firstBracket - 1,
+            promResult
+         );
+      }
+      x--;
+   }
+
+   return calcMany(inArray);
 }
 
-calc("5+(5-5)*1");
+calc("2/-2+   -3 *    4.75- -6");
